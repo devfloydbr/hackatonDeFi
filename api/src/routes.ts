f0b5cf1b1849ce87348cccfae2c4fb66094c8b66
Guilherme J. Floyd <devfloydbr@gmail.com>
2021-11-20 21:12:44 -0300

    TRANSFERTOKEN

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import WalletsController from './controllers/WalletsController'
 import NftsController from './controllers/NftsController'
 import ContractsController from './controllers/ContractsController'
+import SwapController from './controllers/SwapController'
 
 const routes = express.Router()
 
@@ -14,6 +15,7 @@ routes.get('/', (req, res) => {
 routes.post('/wallet', WalletsController.create)
 
 routes.get('/wallet/:address/:protocol', WalletsController.read)
+
 
 // NFT
 routes.post('/nft', NftsController.create)
@@ -28,9 +30,15 @@ routes.get('/symbol/:protocol/:address/:contract', NftsController.symbol)
 
 routes.get('/tokenURI/:protocol/:address/:contract/:tokenId', NftsController.tokenURI)
 
+
 // CONTRACT
 routes.get('/transfersByAddress/:address/:contract/:page/:offset', ContractsController.transfersByAddress)
 
 routes.get('/tokenBalance/:address/:contract', ContractsController.tokenBalance)
+
+routes.post('/transferToken', ContractsController.transferToken)
+
+// SWAP
+routes.post('/createSwap', SwapController.createSwap)
 
 export default routes
